8c609b069a8414b00b9230f06335fe46e5dffddd
RJ <randy.j416@yahoo.com>
2023-05-23 19:06:15 -0400

    Add logic to have hand passed down as prop from app

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,9 +2,18 @@ import './App.css';
 import PlayerHand from './Components/PlayerHand';
 import Rules from './Components/Rules';
 import useHand from './Hooks/useHand' 
+import { useEffect } from 'react';
 
 function App() {
   const { userHand, setUserHand, cpuHand, setCpuHand } = useHand();
+  useEffect(()=> {
+    setUserHand({     card1: { level: 1, points: 0 },
+      card2: { level: 5, points: 1 },
+      card3:{ level: 11, points: 2 },
+      card4:{ level: 13, points: 3 },
+      card5:{ level: 15, points: 4 }, });
+    setCpuHand({ card1: {}, card2: {}, card3: {}, card4: {}, card5: {} });
+  }, [])
   return (
     <div className="App">
       <Rules/>
--- front-end/src/Components/PlayerHand.jsx
+++ front-end/src/Components/PlayerHand.jsx
@@ -4,12 +4,20 @@ export default function PlayerHand (props) {
     console.log("Selected card: ", card.level, card.points);
   };
 
+  // const hand = [
+  //   { level: 1, points: 0 },
+  //   { level: 5, points: 1 },
+  //   { level: 11, points: 2 },
+  //   { level: 13, points: 3 },
+  //   { level: 15, points: 4 },
+  // ];
+
   const hand = [
-    { level: 1, points: 0 },
-    { level: 5, points: 1 },
-    { level: 11, points: 2 },
-    { level: 13, points: 3 },
-    { level: 15, points: 4 },
+    props.cards.card1,
+    props.cards.card2,
+    props.cards.card3,
+    props.cards.card4,
+    props.cards.card5,
   ];
 
   
